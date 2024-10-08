c95120a54373f217654f468e4911ddaf616806ea
seeun00 <63391096+seeun00@users.noreply.github.com>
2024-10-08 10:47:34 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
+
 provider "aws" {
-  version = "~> 2.0"
-  region  = "us-east-1"
+  region     = "us-west-2"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
 
 # Create a VPC
